7b12e44c14117c6cfc63a769821769a03517997c
johnny <15818464582@163.com>
2022-11-14 21:52:32 +0800

    feat: slot init

--- example/helloworld/Foo.js
+++ example/helloworld/Foo.js
@@ -1,17 +1,18 @@
-// import { h } from '.'
-import { h } from '../../lib/mini-vue-esm.js';
+import { h, ref } from '../../lib/mini-vue-esm.js';
 
 export const Foo = {
   setup(props) {
     console.log(props);
     props.fooState++;
+    const count = ref(0);
+
     // todo
     return {
-      
+      count
     }
   },
   render() {
     
-    return  h('div', {} , this.fooState)
+    return  h('div', {} , [h('div', {}, this.count.value + '' + this.fooState)])
   }
 }
--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -1,5 +1,5 @@
 export function initSlots(instance:any,  children: any) {
     console.log('children', children);
     
-    instance.slots = children || [];
+    instance.slots = Array.isArray(children) ? children : [children] 
 }
